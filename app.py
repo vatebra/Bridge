f92415b5f331bd44582296a68b324cc52982fd87
vatebra <vatebra@gmail.com>
2026-05-15 07:31:24 -0800

    Update app.py

--- app.py
+++ app.py
@@ -80,5 +80,5 @@ def health():
     return {"status": "healthy", "message": "WAEC Proxy is running", "endpoint": "/check"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
